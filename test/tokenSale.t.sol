ea9ce266cdb363caa223a458331be7bf8507bd09
TheCreatorNode <basseyu01@gmail.com>
2025-08-28 16:45:09 +0100

    ADD TOKENSALE TEST CONTRACT

---
+++ test/tokenSale.t.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {tokenSale} from "../src/tokenSale.sol";
+
+contract testTokenSale is Test {}
